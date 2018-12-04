4c7a3988cbfc1a44cbb5d24fcdc19f476c205ed9
Alelit4 <alelit4@gmail.com>
2018-12-04 22:13:46 +0000

    listview modifications + router

--- lib/converter_route.dart
+++ lib/converter_route.dart
@@ -14,11 +14,12 @@ class ConverterRoute extends StatelessWidget {
         assert(color != null),
         assert(units != null);
 
-  @override
-  Widget build(BuildContext context) {
+  Widget getList(BuildContext context) {
     final unitWidgets = units.map((Unit unit) {
       return Container(
         color: color,
+        height: 100,
+        width: 200,
         margin: EdgeInsets.all(8.0),
         padding: EdgeInsets.all(16.0),
         child: Column(
@@ -40,5 +41,29 @@ class ConverterRoute extends StatelessWidget {
       children: unitWidgets,
     );
   }
-}
 
+  Widget getBlock() {
+    return Center(
+      child: Container(
+        color: color,
+        height: 600,
+        width: 350,
+        child: Center(
+          child: Text(
+            "Hello " + name,
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 40.0,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+//    return getList(context);
+    return getBlock();
+  }
+}
